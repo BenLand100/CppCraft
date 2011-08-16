afc60587770c5dc75e32a17430683e4a650d0fab
Benjamin Land <benland100@gmail.com>
2011-08-16 19:52:09 -0400

    Licensed with GPLv3, added a brief README, and removed some unneeded temp files

--- resources/cpackets.py
+++ resources/cpackets.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 
+'''
+ *  Copyright 2011 by Benjamin J. Land (a.k.a. BenLand100)
+ *
+ *  This file is part of the CppCraft.
+ *
+ *  CppCraft is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CppCraft is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CppCraft. If not, see <http://www.gnu.org/licenses/>.
+'''
+
 #DON'T OVERRITE THE ACTUAL FILE WITH THIS FILE'S OUTPUT
 #kept because it might be useful if minecraft's protocol
 #ever changes radically (unlikely)
