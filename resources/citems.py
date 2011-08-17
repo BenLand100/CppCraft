e44b765cc43ca6371c59e1cc94860ff4c371f426
Benjamin Land <benland100@gmail.com>
2011-08-16 20:42:28 -0400

    item_passable was inverted, changed the generating script to fix

--- resources/citems.py
+++ resources/citems.py
@@ -37,6 +37,7 @@ while True:
         parts[1] = '(const char*)"'+parts[1]+'"'
         for i in range(2,9):
             parts[i] = 'true' if parts[i] == '1' else 'false'
+        parts[5] = 'true' if parts[5] == 'false' else 'false'
         parts[10] = 'M_'+(parts[10].upper() if parts[10] != 'NoMaterial' else 'NONE')
         items.append(parts)
 
@@ -56,7 +57,28 @@ for parts in items:
     for i in range(1,len(parts)):
         methods[i] += '        case ' + parts[0] + ': return ' + parts[i] + ';\n'
         
-print '#ifndef _items\n#define _items\n','''
+print '''/**
+ *  Copyright 2011 by Benjamin J. Land (a.k.a. BenLand100)
+ *
+ *  This file is part of the CppCraft.
+ *
+ *  CppCraft is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CppCraft is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CppCraft. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _items
+#define _items
+
 #define M_NONE          0
 #define M_STONE         1
 #define M_DIRT          2
